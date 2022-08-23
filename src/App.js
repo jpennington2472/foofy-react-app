f953615954c35fc9ecdf42ced08fd719921f2401
justinpenn <justin.pennington@velocityautomotivesolutions.com>
2022-08-22 21:42:14 -0400

    Removed unused Imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './components/UI/Router.tsx';
-import CostForm from './components/pages/CostForm.tsx';
 function App() {
   return (
     <div className="app-js-component">
--- src/components/UI/Router.tsx
+++ src/components/UI/Router.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blog from "../pages/Blog.tsx";
 import Contact from "../pages/Contact.tsx";
